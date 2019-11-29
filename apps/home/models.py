c928e77560e2966c8d631856032221f5c12af34e
Ryan Trigg <rbutustree@gmail.com>
2019-11-29 14:10:31 -0800

    rebuilt website with new structure

--- apps/home/models.py
+++ apps/home/models.py
@@ -1,7 +1,29 @@
 from django.db import models
 
 from wagtail.core.models import Page
+from wagtail.core.fields import RichTextField
+from wagtail.admin.edit_handlers import FieldPanel, StreamFieldPanel
+from wagtail.core.fields import StreamField
+
+from apps.streams import blocks
 
 
 class HomePage(Page):
-    pass
+
+    content = StreamField(
+        [
+            ('title_and_text', blocks.TitleAndTextBlock(name='title_and_text'))
+        ],
+        null=True,
+        blank=True,
+
+    )
+
+    content_panels = Page.content_panels + [
+        StreamFieldPanel('content'),
+    ]
+
+    class Meta:
+        verbose_name = "Homepage"
+        verbose_name_plural = "Homepage"
+
--- soundbox/settings/base.py
+++ soundbox/settings/base.py
@@ -27,6 +27,7 @@ BASE_DIR = os.path.dirname(PROJECT_DIR)
 
 INSTALLED_APPS = [
     'apps.home',
+    'apps.streams',
     'search',
 
     'wagtail.contrib.forms',
